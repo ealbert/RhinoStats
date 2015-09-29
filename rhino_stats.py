92eaa2db86c0d784f54c997d5a14c76c4c580cf9
Enrique Albert <enrique.albert.gleiser@gmail.com>
2015-09-29 14:09:42 +0100

    Additional clans were added

--- rhino_stats.py
+++ rhino_stats.py
@@ -2,7 +2,7 @@ import csv
 from io import StringIO
 import os
 
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
 from controllers.clan_controller import ClanController
 from repository.db_context import DbContext
 from utils.csv_exporter import CsvExporter, excel_semicolon
@@ -20,16 +20,36 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))  # refers to application_t
 
 
 @app.route('/')
-def clan_stats():
-    clan_id = '500050913' # This is PU clan id
-    players_data = ClanController().get_clan_stats(connect_db(), clan_id)
-    enhanced_players_data = RatingCalculator.enhance_data(players_data)
-    return render_template('player_stats.html', players_data=enhanced_players_data, clan_id=clan_id)
+def home():
+    return render_template('home.html', context='')
 
-@app.route('/csv')
-def clan_stats_csv():
-    clan_id = '500050913' # This is PU clan id
-    players_data = ClanController().get_clan_stats(connect_db(), clan_id)
+
+@app.route('/search_clan', methods=['POST'])
+def search_clan():
+    clan_name = request.form['search']
+    controller = ClanController()
+    search_details = controller.find_clan(clan_name)
+    if search_details:
+        if len(search_details) > 1:
+            msg = '{clans} clans were found - please select one from bellow'.format(clans=str(len(search_details)))
+            return render_template('home.html', context={'msg': msg, 'search_details': search_details,
+                                                         'clan_name': clan_name})
+        else:
+            return _clan_stats(search_details[0]['clan_id'])
+    else:
+        msg = 'Clan was not found with that name or/and tag'
+        return render_template('home.html', context={'msg': msg, 'search_details': [], 'clan_name': clan_name})
+
+@app.route('/clan_stats/<clan_id>')
+def _clan_stats(clan_id):
+    players_data, clan_details = ClanController().get_clan_stats(connect_db(), clan_id)
+    enhanced_players_data = RatingCalculator.enhance_data(players_data)
+    return render_template('player_stats.html', players_data=enhanced_players_data, clan_details=clan_details)
+
+
+@app.route('/csv/<clan_id>')
+def clan_stats_csv(clan_id):
+    players_data, _ = ClanController().get_clan_stats(connect_db(), clan_id)
     data = CsvExporter.get_data_into_list(players_data)
     si = StringIO()
     cw = csv.writer(si, dialect=excel_semicolon)
@@ -38,6 +58,7 @@ def clan_stats_csv():
     output.headers["Content-Disposition"] = "attachment; filename=rhino_stats.csv"
     output.headers["Content-type"] = "text/csv"
     return output
+
 
 @app.route('/rebuild_db')
 def rebuild_db():
@@ -52,6 +73,11 @@ def rebuild_db():
 def connect_db():
     database_path = os.path.join(APP_ROOT, 'database/' + app.config['DATABASE'])
     return DbContext.get_connection(database_path)
+
+
+
+
+
 
 
 if __name__ == '__main__':
