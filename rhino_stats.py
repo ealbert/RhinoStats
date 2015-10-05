cd72c284829360ad82a35be6b097f0bab8d38246
eagleiser <enrique.albert.gleiser@gmail.com>
2015-10-05 10:11:54 -0400

    Add application login

--- rhino_stats.py
+++ rhino_stats.py
@@ -1,8 +1,10 @@
 import csv
 from io import StringIO
+import logging
 import os
 
 from flask import Flask, render_template, make_response, request, redirect, url_for
+import time
 from controllers.clan_controller import ClanController
 from repository.db_context import DbContext
 from utils.csv_exporter import CsvExporter, excel_semicolon
@@ -18,6 +20,13 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))  # refers to application_top
 
+logger = logging.getLogger('myapp')
+log_file = os.path.join(APP_ROOT, 'rhino-stats.log')
+hdlr = logging.FileHandler(log_file)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
 
 @app.route('/')
 def home():
@@ -27,6 +36,7 @@ def home():
 @app.route('/search_clan', methods=['POST'])
 def search_clan():
     clan_name = request.form['search']
+    logger.info('Search for clan: %s' % clan_name)
     controller = ClanController()
     search_details = controller.find_clan(clan_name)
     if search_details:
@@ -44,13 +54,18 @@ def search_clan():
 
 @app.route('/clan_stats/<clan_id>')
 def clan_stats(clan_id):
+    logger.info('Display Stats for clan: %s' % clan_id)
+    start_time = time.time()
     players_data, clan_details = ClanController().get_clan_stats(connect_db(), clan_id)
     enhanced_players_data = RatingCalculator.enhance_data(players_data)
+    end_time = time.time()
+    logger.info('Display Stats Elapsed Time: %s' % str(end_time - start_time))
     return render_template('player_stats.html', players_data=enhanced_players_data, clan_details=clan_details)
 
 
 @app.route('/csv/<clan_id>')
 def clan_stats_csv(clan_id):
+    logger.info('Download CSV for clan: %s' % clan_id)
     players_data, _ = ClanController().get_clan_stats(connect_db(), clan_id)
     data = CsvExporter.get_data_into_list(players_data)
     si = StringIO()
